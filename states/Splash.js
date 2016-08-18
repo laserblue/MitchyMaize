5dde4ba8ec6ebc56ef66eeb99c7107c82f63fea7
laserblue <spectramollisol@yahoo.com>
2016-08-18 09:52:53 -0600

    Update Splash.js
    I added spaces after commas of code in loadScripts and addGameStates. I am hoping that maybe this is what is preventing the main menu from appearing.

--- states/Splash.js
+++ states/Splash.js
@@ -6,9 +6,9 @@ Splash.prototype = {
     game.load.script('style', 'lib/style.js');
     game.load.script('mixins', 'lib/mixins.js');
     game.load.script('WebFont', 'vendor/webfontloader.js');
-    game.load.script('gamemenu','states/GameMenu.js');
+    game.load.script('gamemenu', 'states/GameMenu.js');
     game.load.script('game', 'states/Game.js');
-    game.load.script('gameover','states/GameOver.js');
+    game.load.script('gameover', 'states/GameOver.js');
     game.load.script('credits', 'states/Credits.js');
     game.load.script('options', 'states/Options.js');
   },
@@ -57,11 +57,11 @@ Splash.prototype = {
 
   addGameStates: function () {
 
-    game.state.add("GameMenu",GameMenu);
-    game.state.add("Game",Game);
-    game.state.add("GameOver",GameOver);
-    game.state.add("Credits",Credits);
-    game.state.add("Options",Options);
+    game.state.add("GameMenu", GameMenu);
+    game.state.add("Game", Game);
+    game.state.add("GameOver", GameOver);
+    game.state.add("Credits", Credits);
+    game.state.add("Options", Options);
   },
 
   addGameMusic: function () {
