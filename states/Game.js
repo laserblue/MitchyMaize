db00bfcbb25d7f9aa32a87b1aadb3bdbeb12252d
laserblue <spectramollisol@yahoo.com>
2016-08-30 14:48:54 -0600

    Update Game.js
    added missing final bracket to
     txt.stroke = "rgba(0,0,0,0";

--- states/Game.js
+++ states/Game.js
@@ -10,7 +10,7 @@ Game.prototype = {
     var optionStyle = { font: '30pt TheMinion', fill: 'white', align: 'left', stroke: 'rgba(0,0,0,0)', srokeThickness: 4};
     var txt = game.add.text(game.world.centerX, (this.optionCount * 80) + 200, text, optionStyle);
     txt.anchor.setTo(0.5);
-    txt.stroke = "rgba(0,0,0,0";
+    txt.stroke = "rgba(0,0,0,0)";
     txt.strokeThickness = 4;
     var onOver = function (target) {
       target.fill = "#FEFFD5";
