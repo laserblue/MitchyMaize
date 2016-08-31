fbb00506eedaa940abea418a1c92d550c79dcf36
laserblue <spectramollisol@yahoo.com>
2016-08-31 14:18:56 -0600

    Update main.js
    I probably shouldn't be doing this on the master branch.
    Changed the code to use temporary astroid logo on splash screen.

--- main.js
+++ main.js
@@ -14,12 +14,12 @@ var
 Main.prototype = {
 
   preload: function () {
-    game.load.image('stars',    'assets/images/lossy-page1-800px-NRCSOH07012_-_Ohio_(717402)(NRCS_Photo_Gallery).tif.jpg');
-    game.load.image('loading',  'assets/images/loading.png');
-    game.load.image('brand',    'assets/images/logo.png');
-    game.load.script('polyfill',   'lib/polyfill.js');
-    game.load.script('utils',   'lib/utils.js');
-    game.load.script('splash',  'states/Splash.js');
+    game.load.image('stars', 'assets/images/lossy-page1-800px-NRCSOH07012_-_Ohio_(717402)(NRCS_Photo_Gallery).tif.jpg');
+    game.load.image('loading', 'assets/images/loading.png');
+    game.load.image('brand', 'assets/images/Envelope_astroid.svg.png');
+    game.load.script('polyfill', 'lib/polyfill.js');
+    game.load.script('utils', 'lib/utils.js');
+    game.load.script('splash', 'states/Splash.js');
   },
 
   create: function () {
