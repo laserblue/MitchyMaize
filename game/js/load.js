2325116636e88fc86528cb880b2390aae8bf1bfb
laserblue <spectramollisol@yahoo.com>
2018-06-22 12:52:09 -0600

    Add files via upload

---
+++ game/js/load.js
@@ -0,0 +1,80 @@
+var tileSize = 64;
+var dRows = 10;
+var dCols = 12;
+var dialogue;
+var spaceKey;
+
+var Game = {
+  w: tileSize*dCols,
+  h: tileSize*dRows,
+  camera: {x:0, y:0}
+};
+
+// var Game = {
+//   w: 800,
+//   h: 600
+// };
+
+// var w = 800;
+// var h = 600;
+
+Game.Boot = function(game) {
+  this.game = game;
+};
+
+Game.Boot.prototype = {
+  preload: function() {
+    // console.log('blah'+Game.w);
+		this.game.stage.backgroundColor = '#FFF';
+		this.game.load.image('loading', 'assets/images/loading.png');
+		this.game.load.image('title', 'assets/images/title.png');
+		this.game.load.image('instructions', 'assets/images/instructions.png');
+    this.game.load.bitmapFont('minecraftia', 'assets/fonts/font.png', 'assets/fonts/font.xml'); //load default font
+
+    //Scale Image to Fit Window
+    this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.game.scale.maxHeight = window.innerHeight;
+    this.game.scale.maxWidth = window.innerHeight*(Game.w/Game.h);
+
+  },
+  create: function() {
+   this.game.state.start('Load');
+  }
+};
+
+Game.Load = function(game) {
+  this.game = game;
+};
+
+Game.Load.prototype = {
+  preload: function() {
+    
+    //Debug Plugin
+    // this.game.add.plugin(Phaser.Plugin.Debug);
+
+    //Loading Screen Message/bar
+    var loadingText = this.game.add.bitmapText(Game.w/2, Game.h/2, 'minecraftia', 'Loading...', 30).anchor.setTo(0.5);
+  	var preloading = this.game.add.sprite(Game.w/2-64, Game.h/2+50, 'loading');
+  	this.game.load.setPreloadSprite(preloading);
+
+    this.game.load.spritesheet('player','assets/images/hero_x64.png', 64, 64, 12);
+    //this.game.load.spritesheet('player','assets/images/rpg_sprite_walk.png', 24, 32, 32);
+    
+     
+    this.game.load.spritesheet('tiles','assets/images/tiles2.png', tileSize, tileSize, 19);
+    this.game.load.tilemap('test', 'assets/atlas/test.json',null,Phaser.Tilemap.TILED_JSON);
+    this.game.load.tilemap('test2', 'assets/atlas/test2.json',null,Phaser.Tilemap.TILED_JSON);
+
+
+
+    //Load button for twitter
+    this.game.load.image('twitter','assets/images/twitter.png');
+
+    // Music Track
+    // this.game.load.audio('music','soundtrack.mp3');
+
+  },
+  create: function() {
+    this.game.state.start('Menu');
+  }
+};
---
+++ game/js/play.js
@@ -0,0 +1,128 @@
+/*global Game*/
+
+/**
+ * Returns a random integer between min and max
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+
+// // Choose Random integer in a range
+// function rand (min, max) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+// var musicOn = true;
+
+var wKey;
+var aKey;
+var sKey;
+var dKey;
+var leftArrow;
+var rightArrow;
+var upArrow;
+var downArrow;
+var score = 0;
+
+Game.Play = function(game) {
+  this.game = game;
+};
+
+Game.Play.prototype = {
+  create: function() {
+    this.game.world.setBounds(0, 0 ,Game.w ,Game.h);
+
+    this.map = this.game.add.tilemap('test2');
+    this.map.addTilesetImage('tiles');
+
+    this.layer1 = this.map.createLayer('layer1');
+    this.layer2 = this.map.createLayer('layer2');
+
+    this.map.setCollision([1,7,8,9,10,11]);
+    this.map.setCollision([1,7,8,9,10,11,14,15,16,17],true,'layer2');
+
+    this.layer1.resizeWorld();
+    this.layer2.resizeWorld();
+
+    var tile1 = this.map.getTile(0,0,'layer1');
+    // console.log('tile' + tile1);
+
+    this.player = new Player(this.game, 5, 5, this.map);
+    // this.player = new Player(this.game, 40, 31, this.map);
+
+
+    this.signs = this.game.add.group();
+    this.map.createFromObjects('objects', 19, 'tiles', 18, true, false, this.signs);//yellow
+
+    //Initialize Sign 
+    this.sign_text = this.game.add.bitmapText(32, 480,'minecraftia','', 24);
+    this.sign_text.fixedToCamera = true;
+
+
+    // // Music
+    // this.music = this.game.add.sound('music');
+    // this.music.volume = 0.5;
+    // this.music.play('',0,1,true);
+
+    //Setup WASD and extra keys
+    wKey = this.game.input.keyboard.addKey(Phaser.Keyboard.W);
+    aKey = this.game.input.keyboard.addKey(Phaser.Keyboard.A);
+    sKey = this.game.input.keyboard.addKey(Phaser.Keyboard.S);
+    dKey = this.game.input.keyboard.addKey(Phaser.Keyboard.D);
+    // muteKey = game.input.keyboard.addKey(Phaser.Keyboard.M);
+
+
+    //Create Twitter button as invisible, show during win condition to post highscore
+    // this.twitterButton = this.game.add.button(this.game.world.centerX, this.game.world.centerY + 200,'twitter', this.twitter, this);
+    this.twitterButton = this.game.add.button(45*tileSize, 31*tileSize,'twitter', this.twitter, this);
+    this.twitterButton.anchor.set(0.5);
+    this.twitterButton.visible = true;
+  },
+
+  update: function() {
+
+    //Read Signs
+    this.signs.forEach(function(sign) {
+      if (checkOverlap(this.player, sign)) {
+        this.lastRead = this.game.time.now + 500;
+        this.sign_text.setText(sign.text);
+      }
+    },this);
+
+    if (this.game.time.now > this.lastRead) {
+      this.sign_text.setText('');
+    }
+
+
+    function checkOverlap(spriteA, spriteB) {
+      var boundsA = spriteA.getBounds();
+      var boundsB = spriteB.getBounds();
+      return Phaser.Rectangle.intersects(boundsA, boundsB);
+    }
+
+
+    // // Toggle Music
+    // muteKey.onDown.add(this.toggleMute, this);
+
+  },
+  twitter: function() {
+    //Popup twitter window to post highscore
+    var game_url = 'http://www.divideby5.com/games/making_friends/'; 
+    var twitter_name = 'rantt_';
+    var tags = ['1GAM'];
+
+    window.open('http://twitter.com/share?text=Making+Friends+Is+Easy.+at&via='+twitter_name+'&url='+game_url+'&hashtags='+tags.join(','), '_blank');
+  },
+
+  // toggleMute: function() {
+  //   if (musicOn == true) {
+  //     musicOn = false;
+  //     this.music.volume = 0;
+  //   }else {
+  //     musicOn = true;
+  //     this.music.volume = 0.5;
+  //   }
+  // },
+  // render: function() {
+  //   game.debug.text('Health: ' + tri.health, 32, 96);
+  // }
+
+};
