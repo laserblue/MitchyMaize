e240334f7074c006273fcbf279d95b6a5f1f1a96
laserblue <spectramollisol@yahoo.com>
2018-06-22 12:08:53 -0600

    Add files via upload
    this is the main.js file of the working offline version.

--- game/main.js
+++ game/main.js
@@ -1,25 +1,25 @@
 // Global Variables
-var
-  game = new Phaser.Game(800, 600, Phaser.AUTO, 'game'),
+// (width, height)
+var game = new Phaser.Game(768, 640, Phaser.CANVAS, 'game'), 
+//var game = new Phaser.Game(Game.w, Game.h, Phaser.CANVAS, 'game'), 
+ 
   Main = function () {},
-  gameOptions = {
+   gameOptions = {
     playSound: true,
     playMusic: true
   },
   musicPlayer;
+  
 
-
-
-
-Main.prototype = {
+  Main.prototype = {
 
   preload: function () {
-    game.load.image('stars',    'assets/images/stars.jpg');
-    game.load.image('loading',  'assets/images/loading.png');
-    game.load.image('brand',    'assets/images/logo.png');
-    game.load.script('polyfill',   'lib/polyfill.js');
-    game.load.script('utils',   'lib/utils.js');
-    game.load.script('splash',  'states/Splash.js');
+    game.load.image('stars', 'assets/images/NRCS.jpg');
+    game.load.image('loading', 'assets/images/loading.png');
+    game.load.image('brand', 'assets/images/logo.png');
+    game.load.script('polyfill', 'lib/polyfill.js');
+    game.load.script('utils', 'lib/utils.js');
+    game.load.script('Splash', 'states/Splash.js');
   },
 
   create: function () {
